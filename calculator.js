e89ed47818687e93cbbab7904144dbe5d0907cc7
eungyeolbaek <eungyeolbaek@gmail.com>
2025-12-01 09:19:44 +0900

    Add readme.md

--- calculator.js
+++ calculator.js
@@ -3,3 +3,8 @@ const add = (a, b) => a + b;
 
 //type (x:number, y:number) -> number;
 const substract = (a, b) => a - b;
+
+// git init - 로컬 레포지토리 만들기
+// git add . - stage로 옮기기
+// git commit - 로컬 레포지토리에 저장하기
+// git push - 로컬 레포지토리와 리모트 레포지토리 동기화하기
