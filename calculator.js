99c5964dba1e6482dbc20d1e7d8e26aaa32395b2
eungyeolbaek <eungyeolbaek@gmail.com>
2025-12-01 09:26:39 +0900

    Update multiply func

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const add = (a, b) => a + b;
 //type (x:number, y:number) -> number;
 const substract = (a, b) => a - b;
 
-// git init - 로컬 레포지토리 만들기
-// git add . - stage로 옮기기
-// git commit - 로컬 레포지토리에 저장하기
-// git push - 로컬 레포지토리와 리모트 레포지토리 동기화하기
+// type (x:number, y:number) -> number;
+const multiply = (a, b) => {
+    return a * b;
+}
